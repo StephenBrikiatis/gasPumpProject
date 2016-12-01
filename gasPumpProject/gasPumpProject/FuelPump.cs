a2b35eb05fc3c4c2a7a01b80cff96b4d71fffce2
Brikiatis <stephen.brikiatis@CHAMPLAIN.EDU>
2016-12-01 15:18:05 -0500

    Files created and minor code segments added.

---
+++ gasPumpProject/gasPumpProject/FuelPump.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gasPumpProject
+{
+
+    class FuelPump
+    {
+        Store store;
+        PumpState state;
+        bool inUse;
+        int prepaid;
+        fuelTank tankStatus[];
+
+        public void customerUP()
+        {
+
+        }
+
+        public void employeeUP()
+        {
+
+        }
+
+        public void usePump()
+        {
+
+        }
+    }
+}
---
+++ gasPumpProject/gasPumpProject/UI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gasPumpProject
+{
+    class UI
+    {
+        Store store;
+        
+
+        public void mainInterface()
+        {
+            int userInput;
+            bool exitAction = false;
+            while (exitAction != true)
+            {
+                Console.Out.WriteLine("Press 1 to pay with cash.\n Press 2 to pay with card.\n Input: ");
+                userInput = Console.Read();
+
+                for(int i =0; i < 4; i++)
+                {
+                    store.pumps[i].inUse;
+                }
+
+                if (userInput == 1)
+                {
+                    store.prePayPump();
+                }
+                
+                else
+                {
+                    Console.Out.WriteLine("That input wasn't a recognized input. Please enter a new input.");
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
