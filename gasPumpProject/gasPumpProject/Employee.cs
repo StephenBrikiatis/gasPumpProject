7d7c683126e337a509eb7ffd4d8b3cffad61fe84
JackHouk <jackhouk@hotmail.com>
2016-12-01 15:40:12 -0500

    Initial Classes
    Employee is complete, register is not. Register will not function
    without store and fuelpump.

---
+++ gasPumpProject/gasPumpProject/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//employee entity class, assigns each employee a unique employee ID upon creation
+
+namespace gasPumpProject
+{
+    class Employee
+    {
+        //keeps track of total employees created
+        private static int employees = 0;
+
+        //employee ID
+        private int employeeID { get; }
+
+        //increment number of employees and assign the new employee thier ID
+        public Employee()
+        {
+            employees++;
+            employeeID = employees;
+        }
+
+    }
+}
---
+++ gasPumpProject/gasPumpProject/cashRegister.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Cash register class, tracks which employee is logged in
+//tracks transactions, time amount and employee who was logged in
+//allows for prepay of fuel, employee login and logout
+namespace gasPumpProject
+{
+    class cashRegister
+    {
+        private int registerID { get; }
+        //cash in this register
+        private int currentCash { get; set; }
+        //0 if nobody is logged in
+        private int currentUser;
+        //store that owns this register
+        private Store store;
+        //called from Store.prePayPump() when a customer wants to pay cash for gas
+        //updates cash in register, pump's prepay value, and state of pump (cash mode)
+        //alerts store to the transaction so store can update its records
+        public bool payForFuel(int targetPump, int paymentAmnt)
+        {
+            if (currentUser == 0)
+                return false;
+            currentCash += paymentAmnt;
+            store.receivePumpPay(paymentAmnt);
+            store.fuelPumps[targetPump].prepaid.set(paymentAmnt);
+            store.fuelPumps[targetPump].setState(1);
+            return true;
+        }
+
+    }
+}
